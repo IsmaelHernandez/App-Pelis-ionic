08664ad31b0a9451ab1fb95ae49f49dff117d6ab
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2021-10-08 13:29:24 -0500

    creacionapi-pelis

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  //intectamos servicio de pelis
+  constructor( private http:HttpClient) { }
+
+
+  //primer servicio
+  getFeature(){
+
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-01-31&api_key=bdd9512c3e4ed78025615a3eaf9fa94b&language=es`);
+
+
+  }
+}
