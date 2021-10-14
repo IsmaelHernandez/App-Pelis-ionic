118940d0d77a78feec2afded81ad40659e67f2de
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2021-10-14 12:15:18 -0500

    detallepelis-buscar-peli

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { DetalleComponent } from '../components/detalle/detalle.component';
+import { Pelicula } from '../interfaces/interfaces';
 import { MoviesService } from '../services/movies.service';
 
 @Component({
@@ -9,18 +12,47 @@ import { MoviesService } from '../services/movies.service';
 export class Tab2Page {
 
   textoBuscar = '';
+  peliculas: Pelicula[] = [];
   ideas: string[] = ['Spiderman', 'Avenger', 'El señor de los anillos','La vida es Bella'];
+  buscando = false;
+  
 
-  constructor( private moviesService: MoviesService) {}
+
+  constructor( private moviesService: MoviesService, private modalCtrl: ModalController) {}
 
   //Metodo buscar de tab2-buscar
   buscar( event ) {
 
-    const valor = event.detail.value;
+    const valor: string = event.detail.value;
+
+    if( valor.length === 0){
+      this.buscando = false; //cancelamos el spinner
+      this.peliculas = [];
+      return;
+    }
+
+    this.buscando = true;
+    
+
     this.moviesService.buscarPeliculas(valor)
         .subscribe( resp => {
           console.log(resp);
+          this.peliculas = resp['results'];
+          this.buscando = false;
+          
         });
+  }
+
+  async detalle( id: string){
+    //regresa una promesa
+    const modal = await this.modalCtrl.create({
+      component: DetalleComponent,
+      componentProps: {
+        id
+      }
+    });
+    
+    modal.present();
   }
 
 }
